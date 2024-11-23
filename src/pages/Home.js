88b7d5af3ca0b876fdcc1a7d0bbde41aae592330
Shika <shikatastrophe@gmail.com>
2024-11-22 23:14:10 -0600

    Made an oopsie

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import Products from '../components/Products'
-import Footer from '../components/Footer'
+
 
 const Home = () => {
   return (
     <div className='bg-blue-100'>
         <Banner/>
         <Products/>
-        <Footer/>
     </div>
   )
 }
