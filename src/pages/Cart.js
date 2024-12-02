ace6df1202483abfa4272ec6a64447b9f009a135
Shika <shikatastrophe@gmail.com>
2024-12-02 03:22:30 -0600

    shippo test

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -5,6 +5,7 @@ import { toast, ToastContainer } from 'react-toastify'
 import StripeCheckout from "react-stripe-checkout";
 import  axios  from 'axios';
 import  emailjs  from '@emailjs/browser'
+import  {Shippo}  from 'shippo';
 
 const Cart = () => {
   const productData = useSelector((state)=> state.bazar.productData);
@@ -26,6 +27,82 @@ const Cart = () => {
   },[productData]);
   const handleCheckout = () => {
     if(userInfo){
+      
+      // Create address object
+      const shippoToken = new Shippo({apiKeyHeader: 'shippo_test_f03ba5c63c9171b12d63c423744d20a267796680'});
+      
+      async function run() {
+        const result = await shippoToken.addresses.list();
+      
+        // Handle the result
+        console.log(result);
+      }
+      
+      run();
+      
+      const addressFrom = {
+          name: "TEAM BRIJE",
+          company: "PWIPSTORE",
+          street1: " Xochicalco 678",
+          city: "CDMX",
+          state: "CDMX",
+          zip: "03020",
+          country: "MX", // iso2 country code
+          phone: "+52 55 2317 4934",
+          email: "shikatastrophe@gmail.com",
+      };
+      const addressTo = {
+        name: "Mr Hippo",
+        company: "",
+        street1: "Broadway 1",
+        street2: "",
+        city: "New York",
+        state: "NY",
+        zip: "10007",
+        country: "US",
+        phone: "+1 555 341 9393",
+        email: "mrhippo@shippo.com",
+        metadata: "Hippos dont lie"
+    };
+    var parcel = {
+      "length":"5",
+      "width":"5",
+      "height":"5",
+      "distance_unit":"in",
+      "weight":"2",
+      "mass_unit":"lb",
+  };
+      
+  var shipment = {
+    "object_purpose": "PURCHASE",
+    "address_from": addressFrom,
+    "address_to": addressTo,
+    "parcel": parcel,
+    "async": false
+};
+      
+      const transaction2 = async(token)=>{
+      
+      shippoToken.transaction.create({
+        "shipment": shipment,
+            "carrier_account": "07280f4f96f34cc8b75e593c4835dc38",
+            "servicelevel_token": "usps_priority",
+            "label_file_type": "PNG"
+      }, function (err, transaction) {
+        console.log("transaction : %s", JSON.stringify(transaction, null, 4));
+        // print label_url and tracking_number
+        if (transaction.object_status === "SUCCESS") {
+            console.log("Label URL: %s", transaction.label_url);
+            console.log("Tracking Number: %s", transaction.tracking_number);
+        } else {
+            //Deal with an error with the transaction
+            console.log("Message: %s", transaction.messages);
+        }
+      });
+      }
+
+      transaction2();
+
       var params = {
         to_name : userInfo.displayName,
         from_name : "PWIPSTORE",
